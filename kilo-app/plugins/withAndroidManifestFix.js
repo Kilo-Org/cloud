3e57c60eb3723ff935892e7eacfe6dacaf503c4f
Igor Šćekić <iscekic@protonmail.com>
2026-04-02 21:52:15 +0200

    style: format config plugin

--- kilo-app/plugins/withAndroidManifestFix.js
+++ kilo-app/plugins/withAndroidManifestFix.js
@@ -4,8 +4,8 @@ const { withAndroidManifest } = require('expo/config-plugins');
  * Resolves manifest merger conflict between expo-secure-store and AppsFlyer SDK,
  * which both declare dataExtractionRules and fullBackupContent on <application>.
  */
-const withAndroidManifestFix = (config) => {
-  return withAndroidManifest(config, (config) => {
+const withAndroidManifestFix = config => {
+  return withAndroidManifest(config, config => {
     const application = config.modResults.manifest.application?.[0];
     if (!application) return config;
 
@@ -13,8 +13,7 @@ const withAndroidManifestFix = (config) => {
     config.modResults.manifest.$['xmlns:tools'] = 'http://schemas.android.com/tools';
 
     // Add tools:replace to resolve the conflicting attributes
-    application.$['tools:replace'] =
-      'android:dataExtractionRules,android:fullBackupContent';
+    application.$['tools:replace'] = 'android:dataExtractionRules,android:fullBackupContent';
 
     return config;
   });

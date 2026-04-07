9a1b44a7908c2e9f1ee20f8e9a2670ad36362519
Christiaan Arnoldus <christiaan.arnoldus@outlook.com>
2026-04-07 16:27:30 +0200

    Reformat

--- apps/web/src/lib/providers/direct-byok/direct-byok-definitions.ts
+++ apps/web/src/lib/providers/direct-byok/direct-byok-definitions.ts
@@ -4,4 +4,9 @@ import kimiCoding from './kimi-coding';
 import neuralwatt from './neurowatt';
 import zaiCoding from './zai-coding';
 
-export default [byteplusCoding, kimiCoding, neuralwatt, zaiCoding] satisfies ReadonlyArray<DirectByokProvider>;
+export default [
+  byteplusCoding,
+  kimiCoding,
+  neuralwatt,
+  zaiCoding,
+] satisfies ReadonlyArray<DirectByokProvider>;

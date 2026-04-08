4829edc67c43abc8d7e13912898db08d68668c57
Evan Jacobson <evanjacobson3@gmail.com>
2026-04-08 15:53:55 -0500

    Remove broken comment

--- packages/db/src/migrations/0074_wild_starbolt.sql
+++ packages/db/src/migrations/0074_wild_starbolt.sql
@@ -1,2 +1,2 @@
-ALTER TABLE "kiloclaw_cli_runs" ADD COLUMN "initiated_by" text DEFAULT 'user' NOT NULL;--> statement-breakpoint
+ALTER TABLE "kiloclaw_cli_runs" ADD COLUMN "initiated_by" text DEFAULT 'user' NOT NULL;
 CREATE INDEX "IDX_kiloclaw_cli_runs_initiated_by" ON "kiloclaw_cli_runs" USING btree ("initiated_by");

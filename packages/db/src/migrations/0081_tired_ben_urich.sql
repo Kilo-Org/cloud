103a5d0a79d5360c7581ab79aac8e85bbb95f6c0
Evan Jacobson <evanjacobson3@gmail.com>
2026-04-08 16:49:46 -0500

    Redo migration/generation

--- packages/db/src/migrations/0081_tired_ben_urich.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "kiloclaw_cli_runs" ADD COLUMN "initiated_by" text DEFAULT 'user' NOT NULL;
-CREATE INDEX "IDX_kiloclaw_cli_runs_initiated_by" ON "kiloclaw_cli_runs" USING btree ("initiated_by");

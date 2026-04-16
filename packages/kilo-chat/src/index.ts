20094262d67b8829b627c801a631fbe4b1caa2c5
Igor Šćekić <iscekic@protonmail.com>
2026-04-16 17:21:52 +0200

    feat(kilo-chat-sdk): wire up package exports

--- packages/kilo-chat/src/index.ts
+++ packages/kilo-chat/src/index.ts
@@ -1,1 +1,5 @@
-export {};
+export { KiloChatClient } from './client';
+export { KiloChatSSE } from './sse';
+export { KiloChatApiError } from './errors';
+export { ulidToTimestamp, contentBlocksToText } from './utils';
+export type * from './types';

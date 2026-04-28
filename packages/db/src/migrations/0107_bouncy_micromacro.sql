41ac7b9e7ce12e8fcfa3eb0fec6235f4b91eaf74
St0rmz1 <astorms@replicated.com>
2026-04-28 14:26:02 -0700

    chore(kiloclaw): squash three branch migrations into one

--- packages/db/src/migrations/0107_bouncy_micromacro.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "kilocode_users" ADD COLUMN "kiloclaw_early_access" boolean DEFAULT false NOT NULL;--> statement-breakpoint
-ALTER TABLE "kiloclaw_instances" DROP COLUMN "auto_enroll_in_rollouts";

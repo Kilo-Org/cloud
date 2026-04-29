7fad8792511f0288dc39bc5742a1e2bc7bbe7616
Igor Šćekić <iscekic@protonmail.com>
2026-04-29 18:11:38 +0200

    feat(notifications): add sendPushForConversation WorkerEntrypoint RPC

--- services/notifications/src/__tests__/env.d.ts
+++ services/notifications/src/__tests__/env.d.ts
@@ -1,3 +1,7 @@
+import type NotificationsService from '../index';
+
 declare module 'cloudflare:test' {
-  interface ProvidedEnv extends Env {}
+  interface ProvidedEnv extends Env {
+    SELF: Service<NotificationsService>;
+  }
 }

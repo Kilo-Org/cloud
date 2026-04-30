8fdbc1e2a89498da485810b3375d52464579e22a
Igor Šćekić <iscekic@protonmail.com>
2026-04-30 14:13:27 +0200

    chore(web): remove residual Stream CSS and npm deps

--- apps/web/src/app/(app)/claw/layout.tsx
+++ apps/web/src/app/(app)/claw/layout.tsx
@@ -2,7 +2,6 @@ import { getUserFromAuthOrRedirect } from '@/lib/user.server';
 import { PylonWidget } from '@/components/pylon-widget';
 import { PylonSupportButton } from '@/components/pylon-support-button';
 import { PersonalInstancePresenceMount } from './components/PersonalInstancePresenceMount';
-import './claw-chat.css';
 
 export default async function ClawLayout({ children }: { children: React.ReactNode }) {
   await getUserFromAuthOrRedirect();

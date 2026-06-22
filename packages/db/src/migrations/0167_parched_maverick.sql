36516ab049a4c9d026a10051e2ada079e8f5bca7
Remon Oldenbeuving <remon@kilocode.ai>
2026-06-22 13:55:16 +0200

    fix(organizations): add parent org index concurrently

--- packages/db/src/migrations/0167_parched_maverick.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE "organizations" ADD COLUMN "parent_organization_id" uuid;--> statement-breakpoint
-ALTER TABLE "organizations" ADD CONSTRAINT "organizations_parent_organization_id_organizations_id_fk" FOREIGN KEY ("parent_organization_id") REFERENCES "public"."organizations"("id") ON DELETE restrict ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "organizations" ADD CONSTRAINT "organizations_not_parented_by_self_check" CHECK ("organizations"."parent_organization_id" IS NULL OR "organizations"."parent_organization_id" <> "organizations"."id");

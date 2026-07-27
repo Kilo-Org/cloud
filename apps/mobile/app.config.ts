6fe409cfaf6d3e9f1bcb6204950c0d849a4767e6
Igor Šćekić <iscekic@protonmail.com>
2026-07-27 14:19:54 +0200

    fix(mobile): bump app version to 1.0.4

--- apps/mobile/app.config.ts
+++ apps/mobile/app.config.ts
@@ -26,7 +26,7 @@ const config: ExpoConfig = {
   name: 'Kilo',
   owner: 'kilocode',
   slug: 'kilo-app',
-  version: '1.0.3',
+  version: '1.0.4',
   orientation: 'portrait',
   icon: './assets/images/logo.png',
   scheme: 'kiloapp',
